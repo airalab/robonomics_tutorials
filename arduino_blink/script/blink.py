8281c484ab6b5a68538d8c8d96db1a31893acb9c
Vadim Manaenko <vadim.razorq@gmail.com>
2018-09-12 16:33:53 +0300

    finish the liability

--- arduino_blink/script/blink.py
+++ arduino_blink/script/blink.py
@@ -2,12 +2,20 @@
 import rospy
 import serial
 
+from std_srvs.srv import Empty
 from std_msgs.msg import Empty
 
 def blink(data):
     rospy.loginfo("Blinking...")
     ser = serial.Serial('/dev/ttyUSB0', 9600)
     ser.write(b"blink\n")
+    ser.flush()
+    ser.close()
+
+    rospy.wait_for_service("liability/finish")
+    fin = rospy.ServiceProxy("liability/finish", Empty)
+    rospy.loginfo("finishing...")
+    fin()
 
 def main():
     rospy.init_node("blink_node")
